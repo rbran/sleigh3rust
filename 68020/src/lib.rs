3d66a5f086982b6c4bc5b8841d5550e8c40d3fa2
Rubens Brandao <git@rubens.io>
2023-03-17 12:21:07 -0300

    Add 68000 family

---
+++ 68020/src/lib.rs
@@ -0,0 +1,34 @@
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(unused_assignments)]
+#[allow(unused_parens)]
+#[allow(unused_variables)]
+#[allow(unused_mut)]
+#[allow(unreachable_patterns)]
+#[allow(dead_code)]
+mod disassembler {
+    use sleigh2macro::generate_disasembler;
+    generate_disasembler!("../Processors/68000/data/languages/68020.slaspec");
+}
+use crate::disassembler::*;
+
+use std::fmt::Write;
+
+#[no_mangle]
+pub fn parse_default(
+    tokens: &[u8],
+    inst_start: AddrType,
+) -> Option<(AddrType, String)> {
+    let mut context = SpacesStruct::default();
+    let (addr, parsed) = parse_instruction(
+        tokens,
+        &mut context,
+        inst_start,
+        &mut GlobalSetDefault::<SpacesStruct>::default(),
+    )?;
+    let mut output = String::new();
+    for ele in parsed.into_iter() {
+        write!(&mut output, "{}", ele).unwrap();
+    }
+    Some((addr, output))
+}
---
+++ 68030/build.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let local =
+        std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| "".into());
+    let filename = std::path::Path::new(&local).join("../Processors/68000");
+    println!("cargo:rerun-if-changed={}", filename.to_str().unwrap());
+}
